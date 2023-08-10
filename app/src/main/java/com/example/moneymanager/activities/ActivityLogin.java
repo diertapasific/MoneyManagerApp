de4ecd600883a1c8fae13d0300ae206000d08c99
Dierta Pasific <diertapasific@gmail.com>
2023-08-10 12:35:30 +0700

    Using SQL Database for user account

--- app/src/main/java/com/example/moneymanager/activities/ActivityLogin.java
+++ app/src/main/java/com/example/moneymanager/activities/ActivityLogin.java
@@ -1,80 +1,55 @@
 package com.example.moneymanager.activities;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.moneymanager.R;
+import com.example.moneymanager.database.DataBaseHelper;
 
 public class ActivityLogin extends AppCompatActivity {
 
     TextView btnToRegister;
     EditText email, password;
     Button btnLogin;
-    private SharedPreferences preferences, loginpref;
+    DataBaseHelper databaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-
         email = findViewById(R.id.etEmailLogin);
         password = findViewById(R.id.etPasswordLogin);
         btnLogin = findViewById(R.id.btnLogin);
         btnToRegister = findViewById(R.id.btnToRegister);
+        databaseUser = new DataBaseHelper(this);
 
         btnLogin.setOnClickListener((v) -> {
 
-            String emails = email.getText().toString().trim();
-            String passwords = password.getText().toString().trim();
+            String emails = email.getText().toString();
+            String passwords = password.getText().toString();
 
-            // Validation Email
-            if(emails.isEmpty() && passwords.isEmpty()){
-                Toast toast = Toast.makeText(ActivityLogin.this, "All fields must be filled.", Toast.LENGTH_LONG);
+            if( emails.equals("") || passwords.equals("")){
+                Toast toast = Toast.makeText(ActivityLogin.this, "Please enter all the fields.", Toast.LENGTH_LONG);
                 toast.show();
-            }  else if (!emails.endsWith(".com")) {
-                Toast toast2 = Toast.makeText(ActivityLogin.this, "Email must be ended by .com", Toast.LENGTH_LONG);
-                toast2.show();
-            } else if(emails.isEmpty()){
-                Toast toast = Toast.makeText(ActivityLogin.this, "Please enter your email.", Toast.LENGTH_LONG);
-                toast.show();
-            } else if(passwords.isEmpty()){
-                Toast toast = Toast.makeText(ActivityLogin.this, "Please enter your password.", Toast.LENGTH_LONG);
-                toast.show();
-            }
+            }else{
+                Boolean checkEmailPassword = databaseUser.checkEmailPassword(emails, passwords);
+                if (checkEmailPassword == true){
+                    Toast toast = Toast.makeText(ActivityLogin.this, "Login Successful!", Toast.LENGTH_LONG);
+                    toast.show();
 
-            String savedEmail = preferences.getString("email", "");
-            String savedPassword = preferences.getString("password", "");
-
-            Log.d("email", savedEmail);
-            Log.d("pass", savedPassword );
-
-            if (emails.equals(savedEmail) && passwords.equals(savedPassword)){
-                Intent intent = new Intent(getApplicationContext(), ActivityHome.class);
-                startActivity(intent);
-
-                loginpref = getSharedPreferences("userlogin",MODE_PRIVATE);
-                SharedPreferences.Editor loginedit = loginpref.edit();
-                loginedit.putString("userlogin",emails);
-                loginedit.apply();
-
-                Toast toast = Toast.makeText(ActivityLogin.this, "Login Success!", Toast.LENGTH_LONG);
-                toast.show();
-            } else {
-                // Login failed
-                Toast toast = Toast.makeText(ActivityLogin.this, "Your email address or password incorrect.", Toast.LENGTH_LONG);
-                toast.show();
+                    Intent intent = new Intent(getApplicationContext(), ActivityHome.class);
+                    startActivity(intent);
+                } else{
+                    Toast toast = Toast.makeText(ActivityLogin.this, "Invalid Credentials.", Toast.LENGTH_LONG);
+                    toast.show();
+                }
             }
 
         });
--- app/src/main/java/com/example/moneymanager/activities/ActivityRegister.java
+++ app/src/main/java/com/example/moneymanager/activities/ActivityRegister.java
@@ -1,18 +1,16 @@
 package com.example.moneymanager.activities;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.moneymanager.R;
+import com.example.moneymanager.database.DataBaseHelper;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,38 +20,48 @@ public class ActivityRegister extends AppCompatActivity {
     EditText name, email, password;
     TextView btnToLogin;
     Button btnRegister;
-    private SharedPreferences preferences;
+    DataBaseHelper databaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-
         name = findViewById(R.id.etNameRegister);
         email = findViewById(R.id.etEmailRegister);
         password = findViewById(R.id.etPasswordRegister);
         btnRegister = findViewById(R.id.btnRegister);
         btnToLogin = findViewById(R.id.btnToLogin);
+        databaseUser = new DataBaseHelper(this);
 
-        btnRegister.setOnClickListener((v) -> {
+            btnRegister.setOnClickListener((v) -> {
 
-            String names = name.getText().toString();
-            String emails = email.getText().toString();
-            String passwords = password.getText().toString();
+                String names = name.getText().toString();
+                String emails = email.getText().toString();
+                String passwords = password.getText().toString();
 
-            if (isValid()) {
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putString("username", names);
-                editor.putString("email", emails);
-                editor.putString("password", passwords);
-                editor.apply();
+                if(names.equals("") || emails.equals("") || passwords.equals("")){
+                    Toast toast = Toast.makeText(ActivityRegister.this, "Please enter all the fields.", Toast.LENGTH_LONG);
+                    toast.show();
+                } else{
+                    Boolean checkUser = databaseUser.checkEmail(emails);
+                    if( checkUser == false ){
+                        Boolean insert = databaseUser.insertData(emails, names,passwords);
+                        if( insert == true && isValid()){
+                            Toast toast = Toast.makeText(ActivityRegister.this, "Registered Successfully!", Toast.LENGTH_LONG);
+                            toast.show();
 
-                Toast toast2 = Toast.makeText(ActivityRegister.this, "Account Created!", Toast.LENGTH_LONG);
-                toast2.show();
-                Intent intent2 = new Intent(getApplicationContext(), ActivityLogin.class);
-                startActivity(intent2);
+                            Intent intent = new Intent(getApplicationContext(), ActivityHome.class);
+                            startActivity(intent);
+                        } else{
+                            Toast toast = Toast.makeText(ActivityRegister.this, "Registration Failed!", Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    }
+                    else{
+                        Toast toast = Toast.makeText(ActivityRegister.this, "Account already exists! Please Login.", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
             }
         });
 
@@ -76,26 +84,20 @@ public class ActivityRegister extends AppCompatActivity {
         patternPassword = Pattern.compile(PASSWORD_PATTERN);
         matcherPassword = patternPassword.matcher(passwords);
 
-        // Check if userInput is valid
-        if (names.isEmpty() || emails.isEmpty() || passwords.isEmpty()) {
-            Toast toast = Toast.makeText(ActivityRegister.this, "All fields must be filled.", Toast.LENGTH_LONG);
+        if (names.length() < 5) {
+            Toast toast = Toast.makeText(ActivityRegister.this, "Full Name must be at least 5 characters long.", Toast.LENGTH_LONG);
             toast.show();
             return false;
-        } else if (!names.isEmpty() && !emails.isEmpty() && !passwords.isEmpty()) {
-            if (names.length() < 5) {
-                Toast toast = Toast.makeText(ActivityRegister.this, "Full Name must be at least 5 characters long.", Toast.LENGTH_LONG);
-                toast.show();
-                return false;
-            } else if (!emails.endsWith(".com")) {
-                Toast toast2 = Toast.makeText(ActivityRegister.this, "Email must be ended by .com", Toast.LENGTH_LONG);
-                toast2.show();
-                return false;
-            } else if (!matcherPassword.matches()) {
-                Toast toast = Toast.makeText(ActivityRegister.this, "Password must be alphanumeric & has capital letter.", Toast.LENGTH_LONG);
-                toast.show();
-                return false;
-            }
+        } else if (!emails.endsWith(".com")) {
+            Toast toast2 = Toast.makeText(ActivityRegister.this, "Email must be ended by .com", Toast.LENGTH_LONG);
+            toast2.show();
+            return false;
+        } else if (!matcherPassword.matches()) {
+            Toast toast = Toast.makeText(ActivityRegister.this, "Password must be alphanumeric & has capital letter.", Toast.LENGTH_LONG);
+            toast.show();
+            return false;
         }
+
         return true;
     }
 }
